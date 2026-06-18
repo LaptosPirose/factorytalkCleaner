efa3560cb7a14f35e30d93acc99c754fdcb62c77
Angelo Moura <angelomoura@gmail.com>
2026-06-18 20:56:54 -0300

    Just comment to test

--- src/main/java/com/factorytalkCleaner/FactorytalkCleanerApplication.java
+++ src/main/java/com/factorytalkCleaner/FactorytalkCleanerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FactorytalkCleanerApplication {
 
+	// Main for factory talk Cleaner
 	public static void main(String[] args) {
 		SpringApplication.run(FactorytalkCleanerApplication.class, args);
 	}
